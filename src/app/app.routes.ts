97850f51dcaaf6a59433b3826e49dbe5f84a0b19
Jairo Martinez <ocrensys@gmail.com>
2024-07-27 00:53:15 -0600

    - Implemented the post details component - Integrated the component with routing - Styled the component using Tailwind CSS - Added functionality to navigate back to the previous page - Ensured responsiveness and cross-browser compatibility

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,30 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    // redirectTo: '',
+    redirectTo: 'posts',
     pathMatch: 'full',
+  },
+  {
+    path: 'posts',
     loadComponent: () =>
       import('./features/layout/layout.component').then(
         (module) => module.LayoutComponent
+      ),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/pages/post-list/post-list.component').then(
+            (module) => module.PostListComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: 'details/:id',
+    loadComponent: () =>
+      import('./features/pages/post-details/post-details.component').then(
+        (module) => module.PostDetailsComponent
       ),
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
--- src/app/features/layout/layout.component.ts
+++ src/app/features/layout/layout.component.ts
@@ -2,19 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { PostListComponent } from '../pages/post-list/post-list.component';
 import { SearchbarComponent } from '../../shared/components/searchbar/searchbar.component';
-import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [PostListComponent, SearchbarComponent, NavbarComponent],
+  imports: [
+    RouterOutlet,
+    PostListComponent,
+    SearchbarComponent,
+    NavbarComponent,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
 export class LayoutComponent implements OnInit {
   ngOnInit(): void {
-    console.log('\nLayoutComponent...\n\n\n');
-
     initFlowbite();
   }
 }
