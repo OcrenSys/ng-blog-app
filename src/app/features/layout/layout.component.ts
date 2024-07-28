55bd4f9799a8417f5a2599e4287a4fdf96a27ec6
Jairo Martinez <ocrensys@gmail.com>
2024-07-28 15:13:39 -0600

    style: :zap: Implement global interaction

--- src/app/features/layout/layout.component.ts
+++ src/app/features/layout/layout.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { PostListComponent } from '../pages/post-list/post-list.component';
 import { SearchbarComponent } from '../../shared/components/searchbar/searchbar.component';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { ModalComponent } from '../../shared/components/modal/modal.component';
+import { CardComponent } from '../../shared/components/card/card.component';
+import { DynamicComponentService } from '../../shared/services/dynamic-components/dynamic-component.service';
 
 @Component({
   selector: 'app-layout',
@@ -13,12 +16,25 @@ import { RouterOutlet } from '@angular/router';
     PostListComponent,
     SearchbarComponent,
     NavbarComponent,
+    ModalComponent,
+    CardComponent,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, AfterViewInit {
+  @ViewChild(ModalComponent)
+  modal!: ModalComponent;
+
+  constructor(private dynamicService: DynamicComponentService) {}
+
   ngOnInit(): void {
     initFlowbite();
+  }
+
+  ngAfterViewInit(): void {
+    if (this.modal) {
+      this.dynamicService.modal = this.modal;
+    } else console.log('Modal not found');
   }
 }
--- src/app/features/pages/post-list/post-list.component.ts
+++ src/app/features/pages/post-list/post-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { CardComponent } from '../../../shared/components/card/card.component';
 import { Observable } from 'rxjs';
 import { PostsService } from '../../../shared/services/posts/posts.service';
 import { CommonModule } from '@angular/common';
 import { Post } from '../../../common/interfaces/post.interface';
 import { SpinnerComponent } from '../../../shared/components/spinner/spinner.component';
+import { SigninComponent } from '../../../shared/components/signin/signin.component';
+import { DynamicComponentService } from '../../../shared/services/dynamic-components/dynamic-component.service';
 
 @Component({
   selector: 'app-post-list',
@@ -13,11 +20,16 @@ import { SpinnerComponent } from '../../../shared/components/spinner/spinner.com
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css',
 })
-export class PostListComponent {
+export class PostListComponent implements AfterViewInit {
+  @ViewChild('link', { static: true }) link!: ElementRef<HTMLAnchorElement>;
+
   posts$: Observable<Post[]> = new Observable<Post[]>();
   loading$: Observable<boolean> = new Observable<boolean>();
 
-  constructor(private postService: PostsService) {}
+  constructor(
+    private postService: PostsService,
+    private dynamicComponentService: DynamicComponentService
+  ) {}
 
   ngOnInit(): void {
     this.posts$ = this.postService.posts$;
@@ -26,5 +38,25 @@ export class PostListComponent {
 
   loadMore(): void {
     this.postService.getMorePosts();
+  }
+
+  ngAfterViewInit() {
+    if (this.link) {
+      this.link.nativeElement.addEventListener(
+        'click',
+        this.onLinkClick.bind(this)
+      );
+    } else {
+      console.log('Link element not found');
+    }
+  }
+
+  onLinkClick(event: MouseEvent) {
+    event.preventDefault();
+    this.openModal();
+  }
+
+  openModal() {
+    this.dynamicComponentService.loadComponent(SigninComponent);
   }
 }
